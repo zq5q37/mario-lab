e1d2661ae0f2afe0f7085d7b97bf413e158a5c5c
zq5q37 <zq5q37@gmail.com>
2025-09-30 12:15:53 +0800

    formating

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,23 +12,23 @@ public class CameraController : MonoBehaviour
     private float endX; // largest x-coordinate of the camera
     private float viewportHalfWidth;
 
-    void  Start()
+    void Start()
     {
         // get coordinate of the bottomleft of the viewport
         // z doesn't matter since the camera is orthographic
-        Vector3 bottomLeft =  Camera.main.ViewportToWorldPoint(new  Vector3(0, 0, 0)); // the z-component is the distance of the resulting plane from the camera 
-        viewportHalfWidth  =  Mathf.Abs(bottomLeft.x  -  this.transform.position.x);
-        offset  =  this.transform.position.x  -  player.position.x;
-        startX  =  this.transform.position.x;
-        endX  =  endLimit.transform.position.x  -  viewportHalfWidth;
+        Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)); // the z-component is the distance of the resulting plane from the camera 
+        viewportHalfWidth = Mathf.Abs(bottomLeft.x - this.transform.position.x);
+        offset = this.transform.position.x - player.position.x;
+        startX = this.transform.position.x;
+        endX = endLimit.transform.position.x - viewportHalfWidth;
     }
 
-    void  Update()
+    void Update()
     {
-        float desiredX =  player.position.x  +  offset;
+        float desiredX = player.position.x + offset;
         // check if desiredX is within startX and endX
-        if (desiredX  >  startX  &&  desiredX  <  endX)
-        this.transform.position  =  new  Vector3(desiredX, this.transform.position.y, this.transform.position.z);
+        if (desiredX > startX && desiredX < endX)
+            this.transform.position = new Vector3(desiredX, this.transform.position.y, this.transform.position.z);
     }
 
 }
