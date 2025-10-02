527bb30daf74b07f0109d81da032dc1a929f9c98
zq5q37 <zq5q37@gmail.com>
2025-10-02 16:30:59 +0800

    bricks bounce

--- Assets/Scripts/QuestionBox.cs
+++ Assets/Scripts/QuestionBox.cs
@@ -6,17 +6,19 @@ public class QuestionBox : MonoBehaviour
 
     private bool hasBounced = false;
 
+    private Rigidbody2D rb;
+
     public void resetBounce()
     {
         hasBounced = false;
         questionBoxAnimator.SetBool("enabled", true);
+        rb.bodyType = RigidbodyType2D.Dynamic;
     }
 
     void Start()
     {
-        // update animator state
-        questionBoxAnimator.SetBool("enabled", true);
-        hasBounced = false;
+        rb = GetComponent<Rigidbody2D>();
+        resetBounce();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -31,9 +33,25 @@ public class QuestionBox : MonoBehaviour
             Rigidbody2D playerRb = collision.collider.GetComponent<Rigidbody2D>();
             if (playerRb.linearVelocity.y > 0.1f)
             {
-                hasBounced = true;
-                questionBoxAnimator.SetBool("enabled", false);
+                Bounce();
             }
         }
+    }
+
+    void Bounce()
+    {
+        // give a quick bounce
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+
+        hasBounced = true;
+        questionBoxAnimator.SetBool("enabled", false);
+        // after short delay, turn into static
+        Invoke(nameof(DisableBounce), 0.5f);
+    }
+
+    void DisableBounce()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
     }
 }
