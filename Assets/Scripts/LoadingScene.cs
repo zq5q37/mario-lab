ce5c9a7d4636e68c8321b9c39ef90a126e318bb9
zq5q37 <zq5q37@gmail.com>
2025-10-17 04:05:42 +0800

    loading coroutine

---
+++ Assets/Scripts/LoadingScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public CanvasGroup c;
+
+    void Start()
+    {
+        StartCoroutine(Fade());
+    }
+    IEnumerator Fade()
+    {
+        for (float alpha = 1f; alpha >= -0.05f; alpha -= 0.05f)
+        {
+            c.alpha = alpha;
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+
+        // once done, go to next scene
+        SceneManager.LoadSceneAsync("World-1-1", LoadSceneMode.Single);
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadSceneAsync("Main-Menu", LoadSceneMode.Single);
+        Debug.Log("Return to main menu");
+    }
+}
